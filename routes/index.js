0e17eeed0c4bb4f2dfedc04f75fcc133d138d3e3
Ankit <urankit27@gmail.com>
2019-07-25 10:28:27 +0530

    Instamojo Custom Payments

--- routes/index.js
+++ routes/index.js
@@ -17,4 +17,33 @@ router.get('/home', (req,res,next) => {
 	res.render('home',null)
 })
 
+router.get('/payment', (req,res,next) => {
+	console.log("In Payment");
+
+	var request= require('request');
+
+	var headers = { 'X-Api-Key': 'test_11f6999031ac220a9d8283de47e', 'X-Auth-Token': 'test_704d0f263456e4d325544243963'};
+	var payload = {
+	  purpose: 'FIFA 16',
+	  amount: '2500',
+	  phone: '9999999999',
+	  buyer_name: 'John Doe',
+	  email: 'foo@example.com',
+	  redirect_url: 'http://www.example.com/',
+	  allow_repeated_payments: false
+	};
+	console.log(headers);
+	console.log(payload);
+
+	request.post('https://test.instamojo.com/api/1.1/payment-requests/', {form: payload,  headers: headers}, function(error, response, body){
+	  if(!error && response.statusCode == 201){
+	    console.log(body);
+	    res.send(body);
+	  }
+	  else{
+	  	console.log(response.statusCode);
+	  }
+	})
+})
+
 module.exports = router
